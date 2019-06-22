906238db0f5f4d3a413c731f3185c3f415d54855
iewil <liwei@data.gov.sg>
2019-06-22 17:00:50 +0800

    added more meet to the skeleton - layout - basic file input handling

--- main.js
+++ main.js
@@ -1,17 +1,66 @@
-document.getElementById("input-textarea").addEventListener("click", function(){
-  var countLabel = document.getElementById('cursor-count');
-  var textarea = document.getElementById('input-textarea');
-  var startPosition = textarea.selectionStart;
-  var endPosition = textarea.selectionEnd;
-  var inputLength = textarea.value.length;
+var fileList = []
 
-  // Check if text is provided
-  if (inputLength != 0) {
-    // Check if you've selected text
-    if(startPosition == endPosition){
-      countLabel.innerHTML = "The position of the cursor is (" + startPosition + "/" + inputLength + ")";
-    }else{
-      countLabel.innerHTML = "Selected text from ("+ startPosition +" to "+ endPosition + " of " + inputLength + ")";
+$(document).ready(function() {  
+  $('#input-textarea').val("Add a file on the right")
+
+  $('#input-textarea').click(function() {
+    var countLabel = document.getElementById('cursor-count');
+    var textarea = document.getElementById('input-textarea');
+    var startPosition = textarea.selectionStart;
+    var endPosition = textarea.selectionEnd;
+    var inputLength = textarea.value.length;
+
+    // Check if text is provided
+    if (inputLength != 0) {
+      // Check if you've selected text
+      if(startPosition == endPosition){
+        countLabel.innerHTML = "The position of the cursor is (" + startPosition + "/" + inputLength + ")";
+      } else{
+        countLabel.innerHTML = "Selected text from ("+ startPosition +" to "+ endPosition + " of " + inputLength + ")";
+      }
     }
+  })
+
+    // Check for the various File API support.
+  if (window.File && window.FileReader && window.FileList && window.Blob) {
+
+
+    $("#get-files").on('input', handleFileSelect);
+
+    function clearList(){
+      fileList = []
+      $("#fileList").empty()
+    }
+
+    function addToList(fileName, content){
+      $("#fileList").append("<li>" + fileName +"</li>")
+      fileList.push(content)
+    }
+
+    function handleFileSelect(evt) {
+      clearList()
+      var files = evt.target.files;
+      var output = [];
+      for (var i = 0, f; f = files[i]; i++) {
+        if (!f.type.match('text.*')) {
+          continue;
+        }
+        var reader = new FileReader();
+
+        reader.onload = function(event){
+          var contents = event.target.result;
+          console.log(i)
+          // addToList(files[i].name, contents)
+        };
+
+        reader.readAsText(f);
+      }
+
+    }
+  } else {
+    alert('The File APIs are not fully supported in this browser.');
   }
-},false);
+
+  
+});
+
